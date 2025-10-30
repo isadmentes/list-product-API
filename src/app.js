c2b1d584eddbadb59da57f390ec20905d3be91ce
isadmentes <isadmentes@gmail.com>
2025-10-30 20:56:04 -0300

    Implementado o APP e o Server

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import { env } from './config/env.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+
+export const createApp = () => {
+    const app = express();
+
+    // 1) Middlewares globais
+    app.use(express.json());
+    app.use(helmet());
+    app.use(cors({ origin: env.corsOrigin })); // restrinja em produção
+    app.use(rateLimit({ windowMs: 60_000, max: 100 })); // throttle básico
+
+    // 2) Healthcheck
+    app.get('/health', (_req, res) => res.json({ ok: true }));
+
+    // 4) Error handler (sempre por último)
+    app.use(errorHandler);
+    return app;
+};

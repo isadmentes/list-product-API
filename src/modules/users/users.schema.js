c076258d3f5a055219ee18894b829f353d443e3a
isadmentes <isadmentes@gmail.com>
2025-10-30 21:17:11 -0300

    Elaborado o esquema de validação

---
+++ src/modules/users/users.schema.js
@@ -0,0 +1,19 @@
+// delimitando a validação dos dados//
+import { z } from 'zod';
+
+export const registerSchema = z.object({
+    name: z
+        .string()
+        .min(1, 'name is required'),
+    email: z.
+        email('invalid email'),
+    password: z.string()
+        .min(6, 'password must be at least 6 chars')
+});
+
+export const loginSchema = z.object({
+    email: z
+        .email('invalid email'),
+    password: z
+        .string().min(6, 'password must be at least 6 chars')
+});

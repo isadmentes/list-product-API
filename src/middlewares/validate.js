81952aae2cb765fbbc798dde91f18ce7211599c6
isadmentes <isadmentes@gmail.com>
2025-10-30 21:02:24 -0300

    Implementado o Middleware de validação

---
+++ src/middlewares/validate.js
@@ -0,0 +1,21 @@
+export const validate = (schemas = {}) => (req, _res, next) => {
+    try {
+        if (schemas.body) req.body = schemas.body.parse(req.body);
+        if (schemas.query) req.query = schemas.query.parse(req.query);
+        if (schemas.params) req.params = schemas.params.parse(req.params);
+
+        return next();
+    } catch (err) {
+        const issues = err?.issues?.map(i => ({
+            path: i.path, 
+            message: i.message
+        })) ?? null;
+
+        return next({
+            message: 'Validation error',
+            status: 400,
+            code: 'BAD_REQUEST',
+            details: issues
+        });
+    }
+};

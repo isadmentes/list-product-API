7d0f740a4377d4b0c7ccd3e99895ae7fa2809f35
isadmentes <isadmentes@gmail.com>
2025-11-04 20:38:28 -0300

    Criado as rotas de autenticação

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { makeUserController } from '../modules/users/user.controller.js';
+import { validate } from '../middlewares/validate.js';
+import { registerSchema, loginSchema } from '../modules/users/user.schemas.js';
+
+export const authRouter = () => {
+    const r = Router();
+    const ctrl = makeUserController();
+    r.post('/register', validate({ body: registerSchema }), ctrl.register);
+    r.post('/login', validate({ body: loginSchema }), ctrl.login);
+    r.get('/me', ctrl.me); // protegido internamente no controller (array handler)
+
+    return r;
+};
